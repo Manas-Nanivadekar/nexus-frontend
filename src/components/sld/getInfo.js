2ea114cbd4f574ebb4009591159afe2be4fb9098
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2021-08-18 17:00:01 +0530

    css add

--- src/components/sld/getInfo.js
+++ src/components/sld/getInfo.js
@@ -7,32 +7,90 @@ const URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000/v1/";
 
 const GetInfo = () => {
   const [countryId, setCountryId] = React.useState("");
+  const [data, setData] = React.useState();
+  const [fetched, setFetched] = React.useState(false);
 
-  const getSld = () => {
-    axios
-      .get(`${URL}sld?country_id=${countryId}`)
-      .then((res) => console.log(res.data))
-      .catch((err) => console.error(err));
+  const getSld = async () => {
+    const axiosInfo = await axios.get(`${URL}sld?country_id=${countryId}`);
+    await setData(axiosInfo);
+    await setFetched(true);
   };
 
-  return (
-    <div className="container">
-      <div className="brand-title">
-        <div className="inputs">
-          <input
-            type="text"
-            onChange={(e) => {
-              setCountryId(e.target.value);
-            }}
-          />
-          <label>Country ID</label>
+  if (fetched === false) {
+    return (
+      <div className="container">
+        <div className="brand-title">
+          <div className="inputs">
+            <input
+              type="text"
+              onChange={(e) => {
+                setCountryId(e.target.value);
+              }}
+            />
+            <label>Country ID</label>
+          </div>
+          <button onClick={getSld} type="submit">
+            Submit
+          </button>
         </div>
-        <button onClick={getSld} type="submit">
-          Submit
-        </button>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <>
+        <div>
+          <p
+            style={{
+              display: "flex",
+              backgroundColor: "white",
+              padding: "1rem 0",
+              borderRadius: "0.6rem",
+              margin: "12px 0",
+              color: "#00152A",
+              fontWeight: 700,
+              boxShadow: "0 4px 4px -4px gray",
+              fontSize: "1rem",
+            }}
+          >
+            {" "}
+            {data.data.hash}{" "}
+          </p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            padding: "3rem",
+            borderRadius: "30px",
+            backgroundColor: "#EEF3F5",
+            boxShadow: "0 8px 8px -4px lightblue",
+          }}
+        >
+          {console.log(data)}
+
+          {data.data.event.map((item) => {
+            return (
+              <p
+                style={{
+                  backgroundColor: "white",
+                  padding: "1rem 0",
+                  borderRadius: "0.6rem",
+                  margin: "12px 0",
+                  color: "#00152A",
+                  boxShadow: "0 4px 4px -4px gray",
+                  fontWeight: 700,
+                  fontSize: "1rem",
+                }}
+              >
+                {" "}
+                {`${item}`}{" "}
+              </p>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 };
 
 export default GetInfo;
--- src/components/sld/setInfo.js
+++ src/components/sld/setInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 
 import "../../styles/nexus.css";
+// import Output from "../../pages/output";
 
 const URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000/v1/";
 
@@ -11,7 +12,7 @@ function SetInfo() {
   const [localBankNumber, setLocalBankNumber] = React.useState("");
   const [localBankId, setLocalBankId] = React.useState("");
   const [aliasConversion, setAliasConversion] = React.useState(false);
-  const [aliasFormat, setAliasFormat] = React.useState("");
+  const aliasFormat = "Something";
   const [aliasName, setAliasName] = React.useState("");
   const [aliasDesc, setAliasDesc] = React.useState("");
   const [maxDestValue, setMaxDestValue] = React.useState("");
@@ -19,69 +20,81 @@ function SetInfo() {
     React.useState(false);
   const [payeeType, setPayeeType] = React.useState("");
   const [ipsTimeout, setIpsTimeout] = React.useState(false);
+  const [data, setData] = React.useState();
+  const [fetched, setFetched] = React.useState(false);
 
-  const setSld = () => {
-    axios
-      .post(`${URL}sld?country_id=${countryId}`, {
-        iban: iban,
-        local_bank_number: localBankNumber,
-        local_bank_id: localBankId,
-        alias_conversion: aliasConversion,
-        alias_name: aliasName,
-        alias_format: aliasFormat,
-        alias_desc: aliasDesc,
-        max_destination_value: maxDestValue,
-        account_validation_available: accountValidationAvailable,
-        payee_type: payeeType,
-        ips_timeout: ipsTimeout,
-      })
-      .then((res) => console.log(res.data))
-      .catch((err) => console.error(err));
+  const setSld = async () => {
+    const axiosInfo = await axios.post(`${URL}sld?country_id=${countryId}`, {
+      iban: iban,
+      local_bank_number: localBankNumber,
+      local_bank_id: localBankId,
+      alias_conversion: aliasConversion,
+      alias_name: aliasName,
+      alias_format: aliasFormat,
+      alias_desc: aliasDesc,
+      max_destination_value: maxDestValue,
+      account_validation_available: accountValidationAvailable,
+      payee_type: payeeType,
+      ips_timeout: ipsTimeout,
+    });
+    await setData(axiosInfo.data);
+    await setFetched(true);
   };
 
-  return (
-    <div className="container">
-      <div className="brand-title">BIS</div>
-      <div className="inputs">
-        <label>IBAN</label>
-        <input onChange={(e) => setIban(e.target.value)} type="text" />
-        <label>Country Number</label>
-        <input onChange={(e) => setCountryId(e.target.value)} type="text" />
-        <label>Local Bank Number</label>
-        <input
-          onChange={(e) => setLocalBankNumber(e.target.value)}
-          type="text"
-        />
-        <label>Local Bank Id</label>
-        <input onChange={(e) => setLocalBankId(e.target.value)} type="text" />
-        <label>Alias Conversion</label>
-        <input
-          onChange={(e) => setAliasConversion(e.target.value)}
-          type="text"
-        />
-        <label>Alias Something</label>
-        <input onChange={(e) => setAliasFormat(e.target.value)} type="text" />
-        <label>Alias Name</label>
-        <input onChange={(e) => setAliasName(e.target.value)} type="text" />
-        <label>Alias Description</label>
-        <input onChange={(e) => setAliasDesc(e.target.value)} type="text" />
-        <label>Max Destination Value</label>
-        <input onChange={(e) => setMaxDestValue(e.target.value)} type="text" />
-        <label>Is Account Validation Available</label>
-        <input
-          onChange={(e) => setAccountValidationAvailable(e.target.value)}
-          type="text"
-        />
-        <label>Payee Type</label>
-        <input onChange={(e) => setPayeeType(e.target.value)} type="text" />
-        <label>IPS Timeout</label>
-        <input onChange={(e) => setIpsTimeout(e.target.value)} type="text" />
-        <button onClick={setSld} type="submit">
-          Submit
-        </button>
+  if (fetched === false) {
+    return (
+      <div className="container">
+        <div className="brand-title">BIS</div>
+        <div className="inputs">
+          <label>IBAN</label>
+          <input onChange={(e) => setIban(e.target.value)} type="text" />
+          <label>Country Id</label>
+          <input onChange={(e) => setCountryId(e.target.value)} type="text" />
+          <label>Local Bank Number</label>
+          <input
+            onChange={(e) => setLocalBankNumber(e.target.value)}
+            type="text"
+          />
+          <label>Local Bank Id</label>
+          <input onChange={(e) => setLocalBankId(e.target.value)} type="text" />
+          <label>Alias Conversion</label>
+          <input
+            onChange={(e) => setAliasConversion(e.target.value)}
+            type="text"
+          />
+          <label>Alias Name</label>
+          <input onChange={(e) => setAliasName(e.target.value)} type="text" />
+          <label>Alias Description</label>
+          <input onChange={(e) => setAliasDesc(e.target.value)} type="text" />
+          <label>Max Destination Value</label>
+          <input
+            onChange={(e) => setMaxDestValue(e.target.value)}
+            type="text"
+          />
+          <label>Is Account Validation Available</label>
+          <input
+            onChange={(e) => setAccountValidationAvailable(e.target.value)}
+            type="text"
+          />
+          <label>Payee Type</label>
+          <input onChange={(e) => setPayeeType(e.target.value)} type="text" />
+          <label>IPS Timeout</label>
+          <input onChange={(e) => setIpsTimeout(e.target.value)} type="text" />
+          <button onClick={setSld} type="submit">
+            Submit
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <>
+        <h1> Hello World </h1>
+        {console.log(data)}
+        <p> {data.hash} </p>
+      </>
+    );
+  }
 }
 
 export default SetInfo;
